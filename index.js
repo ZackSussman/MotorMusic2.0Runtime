4161f0ebb1c640d4937bbf451da2ef80247ef5ee
Zack Sussman <zacksussman@Zacks-MacBook-Pro.local>
2026-09-11 14:28:39 -0400

    remove bad dependency

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 export {initializeMotorMusicRuntime} from "./main/src/runtime-business/MotorMusicRuntime.js";
 export {MotorMusicTokensProvider} from './main/generated-javascript/main/src/typescript/MotorMusicTokensProvider.js';
-export {DEFAULT_SYLLABLE_TIME} from './main/generated-javascript/main/src/runtime-business/RuntimeConstants.js';
